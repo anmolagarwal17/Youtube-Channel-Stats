2ce9edf9ff34acd9b1bca4774189e845bbb316a1
Anmol-Agarwal <a.a.rocks17@gmail.com>
2020-12-09 00:17:19 +0530

    app mostly working - cleanup & bug fixes left

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const loginMsg = document.querySelector(".login-msg");
 const channelNameLoader = document.getElementById("channel-name-loader");
 const loadMoreChannelBtn = document.getElementById("load-more-c");
 const ytState = {};
+const channelStats = document.querySelector(".channel-stats");
+const r1 = document.getElementById("r1");
+const r2 = document.getElementById("r2");
+const r3 = document.getElementById("r3");
+const r4 = document.getElementById("r4");
+const r5 = document.getElementById("r5");
+const r6 = document.getElementById("r6");
 
 const CLIENT_ID = "836548214787-m9muhc68m9kr68usnncskdd0to7bov4i.apps.googleusercontent.com";
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
@@ -42,6 +49,7 @@ function initClient() {
 
 // Update UI sign in state changes
 function updateSigninStatus(isSignedIn) {
+	loadMoreChannelBtn.style.display = "none";
 	if (isSignedIn) {
 		loginBtn.style.display = "none";
 		logoutBtn.style.display = "block";
@@ -83,22 +91,143 @@ function handleSignoutClick() {
 	gapi.auth2.getAuthInstance().signOut();
 }
 
+function addNewCard(cardTitle, data, type, row, className = "four") {
+	const card = document.createElement("div");
+	card.classList = `columns card ${className}`;
+	// ! add dynamic class to this(above) to show different card widths
+	const el1 = document.createElement("p");
+	el1.innerHTML = cardTitle;
+	el1.style.textTransform = "capitalize";
+	let el2;
+
+	switch (type) {
+		// 1 - text
+		// 2 - image
+		// 3 - url
+		// 4 - date
+		// 5 - keywords
+		// 6 - wiki links
+		case 1:
+			el2 = document.createElement("p");
+			el2.style.textTransform = "capitalize";
+			el2.innerHTML = data;
+			break;
+		case 2:
+			el2 = document.createElement("a");
+			el2.href = data;
+			el2.target = "_blank";
+			el3 = document.createElement("img");
+			el3.src = data;
+			el2.appendChild(el3);
+			break;
+		case 3:
+			el2 = document.createElement("a");
+			el2.href = `https://www.youtube.com/c/${data}`;
+			el2.innerHTML = `/c/${data}`;
+			break;
+		case 4:
+			el2 = document.createElement("p");
+			el2.innerHTML = data;
+			break;
+		case 5:
+			el2 = document.createElement("p");
+			el2.innerHTML = data;
+			break;
+		case 6:
+			el2 = document.createElement("p");
+			el2.innerHTML = "";
+			data.forEach((element) => {
+				el2.innerHTML += `<a href="${element}">${element}</a>`;
+			});
+			console.log(data);
+			break;
+	}
+	card.appendChild(el1);
+	card.appendChild(el2);
+	row.appendChild(card);
+}
+
+// fetch and show channel stats
+async function showChannelStats(e) {
+	// channelStats
+
+	console.log(e.target.id);
+
+	// hiding channel names
+	channelNames.style.display = "none";
+	loadMoreChannelBtn.style.display = "none";
+	// items(snippet(title, description, customUrl, publishedAt, thumbnails / high / url, country), statistics, topicDetails);
+	// items(snippet / defaultLanguage);
+	// youtube.com / c / traversymedia;
+
+	const query = {
+		part: ["snippet,statistics,topicDetails,status,brandingSettings"],
+		id: [e.target.id],
+		fields: "items(snippet(title,description,customUrl,publishedAt,thumbnails/high/url,country),statistics,topicDetails/topicCategories,status(privacyStatus,madeForKids),brandingSettings(channel/keywords,image))",
+	};
+	console.log(query);
+	try {
+		// items(snippet(title,description,customUrl,publishedAt,thumbnails/high/url,country),
+		//  statistics,topicDetails/topicCategories,status(privacyStatus,madeForKids),
+		// brandingSettings(channel/keywords,image))
+		let result = await gapi.client.youtube.channels.list(query);
+		console.log(result);
+		result = result.result.items[0];
+		// 1 - text
+		// 2 - image
+		// 3 - url
+		// 4 - date
+		// 5 - keywords
+		// 6 - wiki links
+		// channel name
+		addNewCard("Channel name", result.snippet.title, 1, r1);
+		// channel link
+		addNewCard("Channel link", result.snippet.customUrl, 3, r1);
+		// publish date
+		addNewCard("Published date", result.snippet.publishedAt, 4, r1);
+		// channel description
+		addNewCard("Description", result.snippet.description, 1, r2, "");
+		// channel country
+		addNewCard("Country", result.snippet.country, 1, r3);
+		// total videos
+		addNewCard("Total videos", result.statistics.videoCount, 1, r3);
+		// total subscribers
+		if (!result.statistics.hiddenSubscriberCount) addNewCard("Total Subscribers", result.statistics.subscriberCount, 1, r3);
+		else addNewCard("Total subscribers", "Private", 1, r3);
+		// total views
+		addNewCard("Total views", result.statistics.viewCount, 1, r4);
+		// privacy status
+		addNewCard("Privacy status", result.status.privacyStatus, 1, r4);
+		// made for kids
+		addNewCard("Made for kids", result.status.madeForKids, 1, r4);
+		// wiki links
+		addNewCard("Channel category wikipedia links", result.topicDetails.topicCategories, 6, r5, "six");
+		// keywords associated with channel
+		addNewCard("Keywords associated with channel", result.brandingSettings.channel.keywords, 5, r5, "six");
+		// channel icon image with its link
+		addNewCard("Channel icon", result.snippet.thumbnails.high.url, 2, r6);
+		// channel cover image with its link
+		addNewCard("Banner image", result.brandingSettings.image.bannerExternalUrl, 2, r6, "eight");
+		// ! total likes
+		// ! total comments
+	} catch (err) {
+		console.log(err);
+		alert("Something went wront in fetching channel stats.");
+	}
+}
+
 function addChannelNames(items) {
 	for (let i = 0; i < items.length; i++) {
 		const li = document.createElement("li");
 		li.classList.add("h-shadow");
 		li.classList.add("grow");
-		li.id = items[i].id.channelId;
+		li.id = items[i].snippet.channelId;
 		li.innerHTML = `<img src="${items[i].snippet.thumbnails.medium.url}"> ${items[i].snippet.channelTitle}`;
 		setTimeout(() => {
 			channelNames.appendChild(li);
+			li.addEventListener("click", showChannelStats);
 		}, i * 150);
 	}
-	setTimeout(() => {
-		if (channelNames.querySelectorAll("li").length >= ytState.totalResults) loadMoreChannelBtn.disabled = true;
-		console.log(channelNames.querySelectorAll("li").length);
-		console.log(ytState.totalResults);
-	}, 700);
 }
 
 async function getChannels(q = ytState.q, pageToken = ytState.pageToken) {
@@ -108,19 +237,33 @@ async function getChannels(q = ytState.q, pageToken = ytState.pageToken) {
 	// getting 5 channel IDs, title, thumbnails-medium
 	const searchQuery = {
 		part: ["snippet"],
+		maxResults: 5,
 		q,
 		type: ["channel"],
-		pageToken,
+		// pageToken,
+		fields: "nextPageToken,pageInfo/totalResults,items(snippet(channelId,title,description,thumbnails/medium))",
 		// if next page token is empty then first page results are returned
 	};
-
-	const response = await gapi.client.youtube.search.list(searchQuery);
-	ytState.pageToken = response.result.nextPageToken;
-	ytState.totalResults = response.result.pageInfo.totalResults;
-	console.log(response);
-	channelNameLoader.style.display = "none";
-
-	return response.result.items;
+	// const searchQuery = {
+	// 	part: ["snippet"],
+	// 	q,
+	// 	type: ["channel"],
+	// 	pageToken,
+	// 	// if next page token is empty then first page results are returned
+	// };
+	try {
+		const response = await gapi.client.youtube.search.list(searchQuery);
+		ytState.pageToken = response.result.nextPageToken;
+		ytState.totalResults = response.result.pageInfo.totalResults;
+		console.log(response);
+		return response.result.items;
+	} catch (err) {
+		if (err.result.error.code == 403) alert("Today's API quota had been exceeded.");
+		else alert("Something went wront in fetching channels.");
+		throw new Error("Some error orccured");
+	} finally {
+		channelNameLoader.style.display = "none";
+	}
 }
 
 // search channel and display results
@@ -136,15 +279,30 @@ async function searchChannel(e) {
 		channelNameLoader.style.display = "none";
 		return;
 	}
-
-	const items = await getChannels(q);
-	addChannelNames(items);
+	try {
+		const items = await getChannels(q);
+		console.log(items);
+		addChannelNames(items);
+		loadMoreChannelBtn.style.display = "block";
+	} catch (err) {
+		console.log(err);
+		// alert("Some error orccured");
+	}
 }
 
 // load more channels
 async function loadMoreChannels() {
-	const items = await getChannels();
-	addChannelNames(items);
+	// check if there are more results to show
+	if (typeof ytState.pageToken == "undefined") {
+		loadMoreChannelBtn.style.display = "none";
+		return;
+	}
+	try {
+		const items = await getChannels();
+		addChannelNames(items);
+	} catch {
+		// alert("Some error orccured");
+	}
 }
 
 // adding event listeners
